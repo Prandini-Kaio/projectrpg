6c2d0ec6d0536b370097b4621303ca602364fa07
prandini-kaio <prandini.kaio@gmail.com>
2022-11-20 16:00:33 -0300

    Sectionlist, section e alteração na table

---
+++ headers/sectionH.h
@@ -0,0 +1,8 @@
+//
+// Created by kaio on 20/11/22.
+//
+
+#ifndef RPGHELPER_SECTIONH_H
+#define RPGHELPER_SECTIONH_H
+
+#endif //RPGHELPER_SECTIONH_H
---
+++ src/section.c
@@ -0,0 +1,3 @@
+//
+// Created by kaio on 20/11/22.
+//
